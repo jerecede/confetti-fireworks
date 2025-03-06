aa1cd53eb1eec3eca4c9ec9229de1113f6f36041
jeremiascedeno <80321905+jeremiascedeno@users.noreply.github.com>
2025-03-06 22:03:46 +0100

    smanettato con i confetti

--- main.js
+++ main.js
@@ -1,3 +1,38 @@
 import JSConfetti from './node_modules/js-confetti/dist/es/index.js'
-const jsConfetti = new JSConfetti()
-jsConfetti.addConfetti()
+// const jsConfetti = new JSConfetti()
+// jsConfetti.addConfetti()
+
+let canvas = document.getElementById('myCanvas1')
+const jsConfetti1 = new JSConfetti({ canvas })
+jsConfetti1.addConfetti()
+
+canvas = document.getElementById('myCanvas2')
+const jsConfetti2 = new JSConfetti({ canvas })
+jsConfetti2.addConfetti({
+    emojis: ['🌈', '⚡️', '💥', '✨', '💫', '🌸'],
+ })
+
+canvas = document.getElementById('myCanvas3')
+const jsConfetti3 = new JSConfetti({ canvas })
+jsConfetti3.addConfetti({
+    confettiColors: [
+      '#ff0a54', '#ff477e', '#ff7096', '#ff85a1', '#fbb1bd', '#f9bec7',
+    ],
+  })
+
+canvas = document.getElementById('myCanvas4')
+const jsConfetti4 = new JSConfetti({ canvas })
+jsConfetti4.addConfetti({
+    confettiRadius: 6,
+    confettiNumber: 5000,
+  })
+
+canvas = document.getElementById('myCanvas5')
+const jsConfetti5 = new JSConfetti({ canvas })
+jsConfetti5.addConfetti(
+    {
+        emojis: ['🦄'],
+        emojiSize: 100,
+        confettiNumber: 300,
+    }
+)
